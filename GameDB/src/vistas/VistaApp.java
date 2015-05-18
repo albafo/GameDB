ae478e02143fd53d771ece225299c5fee3928a46
albafo <alvaro.bano.fos@gmail.com>
2015-05-18 11:24:15 +0200

    Terminado Estructura GameDB Proyecto 5

--- GameDB/src/vistas/VistaApp.java
+++ GameDB/src/vistas/VistaApp.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JMenu;
+import java.awt.CardLayout;
 
 public class VistaApp extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel contenedorVentanas;
+	private JMenuBar menuBar;
+	private JPanel vistaPrincipal;
 
 	/**
 	 * Launch the application.
@@ -20,13 +26,65 @@ public class VistaApp extends JFrame {
 	 * Create the frame.
 	 */
 	public VistaApp() {
+		
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 440, 440);
-		contentPane = new VistaPrincipal();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
 		
+		creacionMenu();
+		
+		/* Inicializamos las ventanas (JPanel) de nuestra aplicación */
+		
+		vistaPrincipal = new VistaPrincipal();
+		
+		
+		/* Generamos el contenedor de ventanas*/
+		
+		contenedorVentanas = new JPanel();
+		contenedorVentanas.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contenedorVentanas.setLayout(new CardLayout());
+		
+		
+		
+		/* Insertamos en el contendor las ventanas de nuestra aplicación */
+		
+		contenedorVentanas.add(vistaPrincipal);
+		
+		
+		/* Le aplicamos al Jframe el JPanel generado */
+		setContentPane(contenedorVentanas);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * Generamos un menú con una barra de menúJMenuBar,
+	 *  sobre ella tres JMenu y en el primer JMenu un JMenuItem
+	 */
+
+	private void creacionMenu() {
+		
+		menuBar = new JMenuBar();
+		
+		this.setJMenuBar(menuBar);
+		
+		JMenu menuPrincipal = new JMenu("Principal");
+		menuBar.add(menuPrincipal);
+		
+		JMenuItem itemPrincipal = new JMenuItem("Principal");
+		menuPrincipal.add(itemPrincipal);
+		
+		JMenu menuJuegos = new JMenu("Juegos");
+		menuBar.add(menuJuegos);
+		
+		JMenu menuPerfil = new JMenu("Perfil");
+		menuBar.add(menuPerfil);
 	}
 
 }
